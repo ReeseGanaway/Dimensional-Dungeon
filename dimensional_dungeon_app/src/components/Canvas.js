be9a4ddfb8c976594df5fea9db446c96f636d9a4
Reese Ganaway <reeseganaway@gmail.com>
2022-05-24 11:36:12 -0400

    Made it so that the canvas fits the size of the window it is displayed on, and changes size if the window resizes

--- dimensional_dungeon_app/src/components/Canvas.js
+++ dimensional_dungeon_app/src/components/Canvas.js
@@ -8,10 +8,18 @@ const Canvas = (props) => {
     const context = canvas.getContext("2d");
 
     //set properties of the canvas component
-    canvas.width = 1024;
-    canvas.height = 576;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     context.fillStyle = "white";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+    //set all the properties again if the window is resized
+    window.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      context.fillStyle = "white";
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    });
   }, []);
 
   return <canvas ref={canvasReference} {...props} />;
