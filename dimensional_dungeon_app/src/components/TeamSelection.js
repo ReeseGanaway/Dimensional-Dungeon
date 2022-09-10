dacd7ede4ba32a0ccf283d213056ddbcbbaa2222
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2022-09-10 14:51:45 -0400

    Refactored roster so that active heroes are now stored in redux to have characters reappear in the same spots on refresh

--- dimensional_dungeon_app/src/components/TeamSelection.js
+++ dimensional_dungeon_app/src/components/TeamSelection.js
@@ -1,11 +1,14 @@
 import React, { Component, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { modeActions } from "../redux/slices/mode";
+import { rosterActions } from "../redux/slices/roster";
 
 const TeamSelection = (props) => {
   const dispatch = useDispatch();
   const roster = useSelector((state) => state.roster);
   const mode = useSelector((state) => state.mode);
+
+  const collection = roster.collection;
 
   const setTeamSelectionHero = (hero) => {
     dispatch(modeActions.setTeamSelectionHero(hero));
@@ -15,24 +18,30 @@ const TeamSelection = (props) => {
     dispatch(modeActions.setSelectedHero(hero));
   };
 
+  const addHeroActive = (hero) => {
+    dispatch(rosterActions.addHeroActive(hero));
+  };
+
   const displayHero = () => {
-    return Object.keys(roster).map((key) => {
+    return Object.keys(collection).map((key) => {
       return (
         <img
-          key={roster[key].name}
-          id={roster[key].name}
-          src={roster[key].displayIcon}
-          alt={roster[key].displayName}
-          onClick={() => addToTeam(roster[key])}
+          key={collection[key].name}
+          id={collection[key].name}
+          src={collection[key].displayIcon}
+          alt={collection[key].displayName}
+          onClick={() => addToTeam(collection[key])}
         />
       );
     });
   };
 
   const addToTeam = (hero) => {
+    addHeroActive(hero);
     if (props.playerTeam[hero.name]) {
       delete props.playerTeam[hero.name];
     }
+
     setTeamSelectionHero(hero.name);
     setSelectedHero(hero);
 
